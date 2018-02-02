a221f68a49e2759bf94d0cebd16aa14f5d774dce
PDunipace22 <peterdunipace@gmail.com>
2018-02-02 10:29:29 -0500

    Added fields and properties

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -10,6 +10,9 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
+            //create an interactive console window
+            //Prompt user to choose what to do with the virtual pet
+            //Call methods on virtual pet object to do what the user wants
         }
     }
 }
--- VirtualPet/VirtualPet.cs
+++ VirtualPet/VirtualPet.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace VirtualPet
 {
-    class VirtualPet
+    public class VirtualPet
     {
+        //Fields-at least 3
+        private int hunger;
+        private int thirst;
+        private string sleep;
+        private string exercise;
+
+        //Properties-at least 3
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+        public int Thirst
+        {
+            get { return this.thirst; }
+            set { this.thirst = value; }
+        }
+        public string Sleep
+        {
+            get { return this.sleep; }
+            set { this.sleep = value; }
+        }
+        public string Exercise
+        {
+            get { return this.exercise; }
+            set { this.exercise = value; }
+        }
+        //Constructors- at least 1
+        //Methods- at leat 3 including TICK method
     }
 }
