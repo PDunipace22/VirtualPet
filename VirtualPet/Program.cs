4c0a3ce8e73be2bca865840c3daccee9e2f70394
PDunipace22 <peterdunipace@gmail.com>
2018-02-02 17:12:56 -0500

    Added Methods,Interaction Menu

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -13,27 +13,43 @@ namespace VirtualPet
             //create an interactive console window
             //Prompt user to choose what to do with the virtual pet
             //Call methods on virtual pet object to do what the user wants
-
-            int userActivities;
-            VirtualPet userFridge = new VirtualPet(50, 50, 50, 50);
+            // Rupert the possum
+            // Alfred the elephant
+            // Sam the Mynah bird
+            int userChoice;
+            VirtualPet bird = new VirtualPet();
             do
             {
-                Console.WriteLine("Alfred the Elephant");
-                //Console.WriteLine("hunger:");
+                Console.WriteLine("Sam the Mynah Bird");
                 Console.WriteLine("");
-                Console.WriteLine("What would you like to do with your pet?");
-                Console.WriteLine("To give the pet food, press 1.");
-                Console.WriteLine("To give the pet water, press 2.");
-                Console.WriteLine("To have the pet take a nap, press 3.");
-                Console.WriteLine("To play with the pet, press 4.");
-                Console.WriteLine("To do nothing, press 5");
+                Console.WriteLine("How would you like to interact with your bird");
+                Console.WriteLine("1.Feed Sam");
+                Console.WriteLine("2.Take Sam to the vet");
+                Console.WriteLine("3.Talk to Sam");
+                Console.WriteLine("4.Have Sam sleep");
+                Console.WriteLine("5.Do Nothing");
                 Console.WriteLine("");
 
+                userChoice = int.Parse(Console.ReadLine());
+                switch(userChoice)
+                {
+                    case 1:
+                        Console.WriteLine(bird.Feed());
+                        break;
+                    case 2:
+                        Console.WriteLine(bird.Health());
+                        break;
+                    case 3:
+                        Console.WriteLine(bird.Talk());
+                        break;
+                    default:
+                        Console.WriteLine(bird.Sleep());
+                        break;
+                }
 
-
-                userActivities = int.Parse(Console.ReadLine());
-                Console.WriteLine(""); ;
-            } while (userActivities != 5);
+                Console.WriteLine("");
+                
+            } while (userChoice != 5);
         }
-    }
+    } 
 }
--- VirtualPet/VirtualPet.cs
+++ VirtualPet/VirtualPet.cs
@@ -9,31 +9,37 @@ namespace VirtualPet
     public class VirtualPet
     {
         //Fields-at least 3
-        private int hunger;
-        private int thirst;
-        private int sleep;
-        private int exercise;
+        private bool isHungry;
+        private bool isBored;
+        private bool isTired;
+        private bool isSick;
+        private string mood;
 
         //Properties-at least 3
-        public int Hunger
+        public bool IsHungry
         {
-            get { return this.hunger; }
-            set { this.hunger = value; }
+            get { return this.isHungry; }
+            set { this.isHungry = value; }
         }
-        public int Thirst
+        public bool IsBored
         {
-            get { return this.thirst; }
-            set { this.thirst = value; }
+            get { return this.isBored; }
+            set { this.isBored = value; }
         }
-        public int Sleep
+        public bool IsTired
         {
-            get { return this.sleep; }
-            set { this.sleep = value; }
+            get { return this.isTired; }
+            set { this.isTired = value; }
         }
-        public int Exercise
+        public bool IsSick
         {
-            get { return this.exercise; }
-            set { this.exercise = value; }
+            get { return this.isSick; }
+            set { this.isSick = value; }
+        }
+        public string Mood
+        {
+            get { return this.mood; }
+            set { this.mood = value; }
         }
 
         //Constructors- at least 
@@ -41,14 +47,58 @@ namespace VirtualPet
         {
 
         }
-        public VirtualPet(int hunger, int thirst, int sleep, int exercise)
-        {
-            this.hunger = hunger;
-            this.thirst = thirst;
-            this.sleep = sleep;
-            this.exercise = exercise;
-        }
+
 
         //Methods- at leat 3 including TICK method
+        // method for pet's mood
+        public string Feed()
+        {
+            if (IsHungry == false)
+            {
+                IsHungry = true;
+                return "You fed pet.";
+            }
+            else
+            {
+                return "pet has already eaten.";
+            }
+        }
+        public string Talk()
+        {
+            if (IsBored == false)
+            {
+                IsBored = true;
+                return "You just had a conversation with Sam.";
+            }
+            else
+            {
+                return "Sam is already talking to itself.";
+            }
+        }
+        public string Sleep()
+        {
+            if (IsTired == false)
+            {
+                IsTired = true;
+                return "Sam just had a nap.";
+            }
+            else
+            {
+                return "Sam already took a nap.";
+            }
+        }
+        public string Health()
+        {
+            if (IsSick == false)
+            {
+                IsSick = true;
+                return "Sam just got back from the vet.";
+            }
+            else
+            {
+                return "Sam already went to the vet.";
+            }
+        }
+        
     }
 }
